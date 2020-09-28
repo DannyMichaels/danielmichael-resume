b3b980492674bd0cb5a84dc042a667dde7423beb
Daniel Michael <daniel@daniels-air.myfiosgateway.com>
2020-09-28 12:29:14 -0400

    rainbowText

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 #! /usr/bin/env node
 const fs = require('fs')
+var colors = require('colors/safe');
+colors.enable();
 
 fs.readFile(__dirname + '/info.json', 'utf8', function(err, data) {
     if (err) {
         console.log(err)
     } else {
-        console.log(data)
+        console.log(colors.rainbow(data))
         return data
     }
 })
 
-"bin": {
-  "resume-me": "./index.js"
-},
+// "bin": {
+//   "resume-me": "./index.js"
+// },
